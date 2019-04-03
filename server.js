7833347b35585330418869a45e76ec0ca71bfed7
Andrew Wetmore <Alwaygetgo@gmail.com>
2019-04-03 10:59:59 -0400

    Fixed issue with static files

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const knex = require('knex')({
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static('public'));
+app.use(express.static(__dirname +'/public'));
 
 // Handle any socket connections
 io.on('connection', function(socket){
